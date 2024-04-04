5953b6b26a5c03b1df3cbfd87144d9d9dfb04507
eminentetim <etimeminent@gmail.com>
2024-04-04 01:20:35 +0100

    hmmmm

--- ecommerce/project/accounts/admin.py
+++ ecommerce/project/accounts/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Accounts
-admin.site.register(Accounts)
+from .models import CustomUser
+admin.site.register(CustomUser)
--- ecommerce/project/accounts/models.py
+++ ecommerce/project/accounts/models.py
@@ -1,7 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
 
 # Create your models here.
-class Accounts(models.Model):
-    username = models.CharField(max_length=20)
-    email = models.EmailField()
-    password = models.CharField(max_length=20)
+#class Accounts(models.Model):
+ #   username = models.CharField(max_length=20)
+  #  email = models.EmailField()
+   # password = models.CharField(max_length=20)
+    
+
+class CustomUser(AbstractUser):
+    name = models.CharField(null=True, max_length=100, blank=True)
